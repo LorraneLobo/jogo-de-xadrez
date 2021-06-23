ac68cc2b68d2a43ca91447195348c8dda46f89f0
Lorrane Alves <Lorrane.dev@gmail.com>
2021-06-23 14:48:56 -0300

    Chess layer e imprimindo o tabuleiro

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -2,11 +2,13 @@ package model.application;
 
 import model.boardgame.Board;
 import model.boardgame.Position;
+import model.chess.ChessMatch;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
